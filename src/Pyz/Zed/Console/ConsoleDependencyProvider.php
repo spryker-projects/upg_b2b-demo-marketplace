4cc423602e548404849b37183408de90fd37f036
upgrader <upgrader@spryker.com>
2023-11-14 11:10:58 +0000

    Applied release group `CC-29874 Updated `npm` dependencies.`, RG link https://api.release.spryker.com/release-group/5004

--- src/Pyz/Zed/Console/ConsoleDependencyProvider.php
+++ src/Pyz/Zed/Console/ConsoleDependencyProvider.php
@@ -123,6 +123,7 @@ use Spryker\Zed\RestRequestValidator\Communication\Console\RemoveRestApiValidati
 use Spryker\Zed\Router\Communication\Plugin\Console\BackendGatewayRouterCacheWarmUpConsole;
 use Spryker\Zed\Router\Communication\Plugin\Console\BackofficeRouterCacheWarmUpConsole;
 use Spryker\Zed\Router\Communication\Plugin\Console\MerchantPortalRouterCacheWarmUpConsole;
+use Spryker\Zed\Router\Communication\Plugin\Console\RouterCacheWarmUpConsole;
 use Spryker\Zed\SalesInvoice\Communication\Console\OrderInvoiceSendConsole;
 use Spryker\Zed\SalesOms\Communication\Console\ImportOrderItemsStatusConsole;
 use Spryker\Zed\Scheduler\Communication\Console\SchedulerCleanConsole;
@@ -393,6 +394,7 @@ class ConsoleDependencyProvider extends SprykerConsoleDependencyProvider
             new MessageBrokerWorkerConsole(),
             new ScopeCacheCollectorConsole(),
             new DateTimeProductConfiguratorBuildFrontendConsole(),
+            new RouterCacheWarmUpConsole(),
         ];
 
         $propelCommands = $container->getLocator()->propel()->facade()->getConsoleCommands();
